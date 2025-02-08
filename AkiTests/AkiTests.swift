e06d49e6280072114bd521949080bf886f02df59
jckhang <yuxiang.zhang42@gmail.com>
2025-02-08 13:26:55 +0800

    Initial Commit

---
+++ AkiTests/AkiTests.swift
@@ -0,0 +1,17 @@
+//
+//  AkiTests.swift
+//  AkiTests
+//
+//  Created by JYXC- DZ-0100219 on 2025/2/8.
+//
+
+import Testing
+@testable import Aki
+
+struct AkiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
